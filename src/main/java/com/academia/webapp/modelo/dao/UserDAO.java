9b598438df7bf42148083bff228f851dec4d538c
javaee <javaee@javaee-VirtualBox>
2020-09-14 12:31:56 +0200

    subida inicial

--- src/main/java/com/academia/webapp/modelo/dao/UserDAO.java
+++
@@ -1,9 +0,0 @@
-package com.games.webapp.modelo.dao;
-
-import com.games.webapp.modelo.pojo.User;
-
-public interface UserDAO {
-	
-	//Devuelve null si no encuentra el usuario
-	User exists (String name, String password);
-}
